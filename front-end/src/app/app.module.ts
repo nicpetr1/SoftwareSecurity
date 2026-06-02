3d163176a792670a64b2d7a7d90252d8aa13f7bd
nicpetr1 <nicol.petrone@studenti.unina.it>
2026-06-02 10:09:45 +0200

    Upgrade Angular to v18 and dependencies
    Bump Angular core/cli/compiler and related packages to 18.x, update devDependencies, and regenerate package-lock.json. Also apply minimal changes in app.module.ts and package.json to ensure compatibility with the updated Angular toolchain. After this change run npm install and a full build/test pass; follow Angular migration guides if any additional API updates are required.

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarrelloComponent } from './components/carrello/carrello.component';
 import { ListaProdottiComponent } from './components/lista-prodotti/lista-prodotti.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AggiungiProdottoComponent } from './components/aggiungi-prodotto/aggiungi-prodotto.component';
 import { KeycloakService } from './services/keycloak.service';
@@ -16,28 +16,22 @@ export function kcFactory(kcService: KeycloakService){
   return () => kcService.init();
 }
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    CarrelloComponent,
-    ListaProdottiComponent,
-    AggiungiProdottoComponent,
-    ModificaProdottoComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
-  ],
-  providers: [   
-    {
-        provide: APP_INITIALIZER,
-        deps: [KeycloakService],
-        useFactory: kcFactory,
-        multi:true
-    }
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        CarrelloComponent,
+        ListaProdottiComponent,
+        AggiungiProdottoComponent,
+        ModificaProdottoComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule], providers: [
+        {
+            provide: APP_INITIALIZER,
+            deps: [KeycloakService],
+            useFactory: kcFactory,
+            multi: true
+        },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
